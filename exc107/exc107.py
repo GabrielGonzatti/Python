5ed63e8b4ac3c0342c48921a0cea540e9c5e5d92
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2024-07-17 21:21:19 -0300

    atualizando exercícios

--- exc107/exc107.py
+++ exc107/exc107.py
@@ -4,8 +4,8 @@ Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(),
 import moeda
 
 p = float(input('Digite o preço: R$'))
-taxa = float(input('Caso queira aumentar ou diminuir, informe a taxa: R$'))
-print(f'A metade de {p} é {moeda.moeda(moeda.metade(p))}')
-print(f'O dobro de {p} é {moeda.moeda(moeda.dobro(p))}')
-print(f'O Aumento de {p} é {moeda.moeda(moeda.aumentar(p))}')
-print(f'A diminuição de {p} é {moeda.moeda(moeda.diminuir(p))}')
+moeda.resumo(p)
+# print(f'A metade de {p} é {moeda.metade(p, True)}')
+# print(f'O dobro de {p} é {moeda.dobro(p, True)}')
+# print(f'O Aumento de {p} é {moeda.aumentar(p,10, True)}')
+# print(f'A diminuição de {p} é {moeda.diminuir(p,13, True)}')
